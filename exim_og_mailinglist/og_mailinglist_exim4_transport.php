32cc8f421d91fd3bfbd5457c5ead04cc25127999
Kyle Mathews <mathews.kyle@gmail.com>
2010-09-03 12:40:44 -0600

    Big changes. Removing in pieces the drush dependency for handling incoming email and instead moving to using a POST system inspired by Mimemail. Adopting to this will require replacing your og_mailinglist_exim4_transport.php file w/ the new one.

--- exim_og_mailinglist/og_mailinglist_exim4_transport.php
+++ exim_og_mailinglist/og_mailinglist_exim4_transport.php
@@ -9,22 +9,42 @@ while (!feof($fd)) {
   $raw_email .= fread($fd, 1024);
 }
 
-// Load site aliases
-require_once('site_aliases.php');
-$sites = og_mailinglist_site_aliases();
-
 // Set command line arguments (sent by the exim4 transport) to variables.
 $mail_username = $argv[1];
 $mail_domain = $argv[2];
- 
-foreach ($sites as $domain => $drush_alias) {
+
+// Load sites
+require_once('site_post_urls.php');
+$sites = og_mailinglist_site_post_urls();
+
+$post_url = "";
+foreach ($sites as $domain => $url) {
   if (strtolower($mail_domain) === $domain) {
-    // Save the email to file. We would just pass the email directly through
-    // bash but as it turns out, there's a size limit to how big bash arguments
-    // can be and emails with large attachment's exceed that limit.
-    $rand_filename = "/tmp/" . rand(1000, 10000000);
-    file_put_contents($rand_filename, $raw_email . "\n");
-    exec("drush " . $drush_alias . " ogm-post-email " . $rand_filename . " " . $mail_username,
-         $result);
+    $post_url = $url;
   }
 }
+
+if (empty($post_url)) {
+  echo "Could not match the email domain with a Drupal site";
+  exit();
+}
+
+$token = md5('Set this at admin/settings/og_mailinglist then paste here.' . $raw_email . $mail_username);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_URL, $post_url);
+
+//prepare the field values being posted to the service
+$data = array(
+  'message' => $raw_email,
+  'mail_username' => $mail_username,
+  'token' => $token,
+);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+//make the request
+curl_exec($ch);
